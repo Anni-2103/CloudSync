6471acfdce1e9289c11a15290c5e9a2831e5b19a
anirudha <anirudhakesarkar0@gmail.com>
2024-12-06 12:23:18 +0530

    final commit

--- config/db.js
+++ config/db.js
@@ -1,11 +1,12 @@
+ 
 const mongoose = require('mongoose');
 
-function connectToDB(){
-    mongoose.connect(process.env.MONGO_URI).then(()=>{
+function connectToDB() {
+    mongoose.connect(process.env.MONGO_URI).then(() => {
         console.log('Connected to DB');
-    })
-
-
-} 
+    }).catch((error) => {
+        console.error('Error connecting to DB:', error);
+    });
+}
 
 module.exports = connectToDB;
